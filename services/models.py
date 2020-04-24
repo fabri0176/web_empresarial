a093a8f3ccc328f436ec5a52cce8f3f74c25b548
Nestor Parra <fabri0176@gmail.com>
2020-04-24 17:29:02 -0500

    Commit inicial

---
+++ services/models.py
@@ -0,0 +1,23 @@
+import uuid
+from django.db import models
+
+# Create your models here.
+class Services(models.Model):
+    
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField(verbose_name='Titulo', max_length=200)
+    subtitle = models.CharField(verbose_name='Subtitulo', max_length=200)
+    content = models.TextField(verbose_name='Contenido')
+    image = models.ImageField(verbose_name='Imagen', upload_to='services')
+    created = models.DateTimeField(verbose_name='Creado', auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    deleted = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
+    
+
+    class Meta:
+        verbose_name = "Servicio"
+        verbose_name_plural = "Servicios"
+        ordering = ["-created"] #ordenado de descendente - 
