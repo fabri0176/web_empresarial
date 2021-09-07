01aa9d847f1bba5016ca99f45746af7b9fed3b99
Néstor Fabricio Parra <nparra@existaya.com>
2021-09-06 20:32:50 -0500

    Ajuste urls Blog, ajuste tag fecha de blog - formato

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
 
 
 def blog(request):
-    return render(request, 'blog/blog.html')
+    posts = Post.objects.all()
+    return render(request, 'blog/blog.html', {'posts': posts})
